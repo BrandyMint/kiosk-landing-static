566361735dbf80896b1e6943bbac582e51dcde76
Denis Khabarov <admin@saymon21-root.pro>
2015-09-20 12:06:36 +0300

    changed staging servers

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,6 +1,6 @@
 set :stage, :staging
 set :deploy_to, ->{"/home/wwwkiiiosk/#{fetch(:application)}"}
-server 'icfdev.ru', user: 'wwwkiiiosk', port: 250
-server 'icfdev.ru', user: 'wwwkiiiosk', port: 251
+server 'icfdev.ru', user: 'wwwkiiiosk', port: 253
+server 'icfdev.ru', user: 'wwwkiiiosk', port: 254
 
 set :branch, ENV['BRANCH'] || 'master'
